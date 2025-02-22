35b859946b3ce7e817594b0ff742ca15cbd18606
Mohak-Mittal <mohakmittal012@gmail.com>
2025-02-22 20:31:22 +0530

    Added the Layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,24 @@
-import React from "react"
-import {BrowserRouter, Navigate,Routes,Route} from 'react-router-dom'
-import LoginPage from "./pages/Login/LoginPage.jsx"
-import HomePage from "./pages/Home/UploadPYQ.jsx"
-// import RegisterPage from "./pages/RegisterPage.jsx"
-import PageNotFound from "./pages/PageNotFound/PageNotFound.jsx"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/Login/LoginPage.jsx";
+// import RegisterPage from "./pages/RegisterPage.jsx";
+import PageNotFound from "./pages/PageNotFound/PageNotFound.jsx";
+import UploadPYQ from "./pages/Home/UploadPYQ.jsx";
+import Layout from "./Layout/Homelayout.jsx";
 
 function App() {
   return (
-    <>
-    <div className="App">
-      <BrowserRouter>
-    <Routes>
-      {/* <Route path='/' element={<Navigate to="/login"/>}/> */}
-      <Route path='/login' element={<LoginPage/>}/>
-      <Route path='/' element={<HomePage/>}/>
-      {/* <Route path='/register' element={<RegisterPage/>}/> */}
-      <Route path='*' element={<PageNotFound/>}/>
-    </Routes>
-    </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<UploadPYQ />} />
+        </Route>
 
-    </>
-  )
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
