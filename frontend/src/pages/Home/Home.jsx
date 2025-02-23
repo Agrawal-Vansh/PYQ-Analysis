495abf87576772631181c6d1e2db1a3e3b399ead
Agrawal-Vansh <vanshaggrawal1@gmail.com>
2025-02-23 20:53:20 +0530

    Updated Readme.md

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -193,7 +193,7 @@ function Home() {
     <div className="min-h-screen flex flex-col items-center bg-[#1E1E2E] p-8">
       <h1 className="text-4xl font-bold text-white px-8 py-3 rounded-lg shadow-lg 
                      bg-gradient-to-r from-[#3B82F6] to-[#9333EA]">
-        PDF Text Extractor
+        PYQ Question Anaylizer
       </h1>
 
       <div className="bg-[#2A2A3A] p-6 my-6 w-full max-w-lg shadow-lg rounded-xl border border-[#3B3B4F]">
@@ -235,7 +235,7 @@ function Home() {
               generatePotentialQuestions();
             }
           }}
-          className="px-4 py-2 bg-[#9333EA] text-white rounded-md shadow-md mt-4 hover:bg-[#7E22CE]"
+          className=" mt-8 text-4xl font-bold text-white px-8 py-3 rounded-lg shadow-lg bg-gradient-to-r from-[#3B82F6] to-[#9333EA]"
         >
           Generate Potential Questions
         </button>
